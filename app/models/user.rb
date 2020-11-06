9f60735f80efcecea771caa8422ab09ab4cbadb6
Danke1732 <s0lfa1732@gmail.com>
2020-11-06 15:21:39 +0900

    userモデル 入力制限バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :email, uniqueness: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_katakana, presence: true
-  validates :last_name_katakana, presence: true
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i, message: "Include both letters and numbers" }
   validates :birth_date, presence: true
+  with_options presence: true do
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters"}
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters"}
+    validates :last_name_katakana, format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters"}
+    validates :first_name_katakana, format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters"}
+  end
+
 end
