614f1a715c69f609cb5c1dae7e0c6dd7450da14e
Danke1732 <s0lfa1732@gmail.com>
2020-11-06 19:29:53 +0900

    正規表現を変数へ代入設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  NAME_REGEX_KANJI = /\A[ぁ-んァ-ン一-龥]/
+  NAME_REGIX_KATAKANA = /\A[ァ-ヶー－]+\z/
+
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i, message: 'Include both letters and numbers' }
   with_options presence: true do
     validates :nickname
     validates :birth_date
-    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters' }
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters' }
-    validates :last_name_katakana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' }
-    validates :first_name_katakana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' }
+    validates :last_name, format: { with: NAME_REGEX_KANJI, message: 'Full-width characters' }
+    validates :first_name, format: { with: NAME_REGEX_KANJI, message: 'Full-width characters' }
+    validates :last_name_katakana, format: { with: NAME_REGIX_KATAKANA, message: 'Full-width katakana characters' }
+    validates :first_name_katakana, format: { with: NAME_REGIX_KATAKANA, message: 'Full-width katakana characters' }
   end
 end
