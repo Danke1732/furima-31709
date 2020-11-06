42b0280db84c3867ab62cf507dfc93e3a1acb8a8
Danke1732 <s0lfa1732@gmail.com>
2020-11-06 19:09:41 +0900

    空白時バリデーションwith_options内へ移動修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i, message: 'Include both letters and numbers' }
-  validates :birth_date, presence: true
   with_options presence: true do
+    validates :nickname
+    validates :birth_date
     validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters' }
     validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters' }
     validates :last_name_katakana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' }
