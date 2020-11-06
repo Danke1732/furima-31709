d1156743c2878c7dcbe58ba99f67d649f9f1e9bf
Danke1732 <s0lfa1732@gmail.com>
2020-11-06 15:40:38 +0900

    単体テストコードの準備（gemの導入・設定）

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname      {Faker::Name.last_name}
+    email         {Faker::Internet.free_email}
+    password      {Faker::Internet.password(min_length: 15)}
+    password_confirmation      {password}
+    last_name     {Gimei.last.kanji}
+    first_name    {Gimei.first.kanji}
+    last_name_katakana    {Gimei.last.katakana}
+    first_name_katakana    {Gimei.first.katakana}
+    birth_date    {Faker::Date.in_date_period}
+  end
+end
