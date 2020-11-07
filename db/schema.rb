80df4e17b8583438c5b297c1d97833d4da21d816
Danke1732 <s0lfa1732@gmail.com>
2020-11-07 22:34:38 +0900

    itemsテーブルのtransport_days_idカラム名の変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_07_022301) do
+ActiveRecord::Schema.define(version: 2020_11_07_131659) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -40,10 +40,11 @@ ActiveRecord::Schema.define(version: 2020_11_07_022301) do
     t.integer "status_id", null: false
     t.integer "shipping_id", null: false
     t.integer "prefecture_id", null: false
-    t.integer "transport_days_id", null: false
+    t.integer "transportday_id", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "price"
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
