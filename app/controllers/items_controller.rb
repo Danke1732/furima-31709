8007cfe3ae63b861085c8f497132f29b39b02126
Danke1732 <s0lfa1732@gmail.com>
2020-11-07 22:46:02 +0900

    出品機能のコントローラーの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,8 +3,21 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(create_item)
+    binding.pry
+    if @item.save
+      redirect_to action: :index
+    else
+      render action: :new
+    end
+  end
+
+  private
+  def create_item
+    params.require(:item).permit(:name, :image, :description, :category_id, :status_id, :shipping_id, :prefecture_id, :transportday_id, :price).merge(user_id: current_user.id)
   end
 end
