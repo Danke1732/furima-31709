939e951751679cc78987836eeff9ce4d1cf63e78
Danke1732 <s0lfa1732@gmail.com>
2020-11-08 14:04:53 +0900

    itemモデルへバリデーションの設定追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,19 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :image
+    validates :category_id, numericality: { other_than: 1, message: 'Select' }
+    validates :status_id, numericality: { other_than: 1, message: 'Select' }
+    validates :shipping_id, numericality: { other_than: 1, message: 'Select' }
+    validates :prefecture_id, numericality: { other_than: 1, message: 'Select' }
+    validates :transportday_id, numericality: { other_than: 1, message: 'Select' }
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: 'Out of setting range' }
+  end
+  validates :price, numericality:  { only_integer: true, message: 'Half-width number or No decimal point' }
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :status
