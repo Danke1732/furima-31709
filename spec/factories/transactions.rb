5358b18b0ea9737f305d36dfb251ff80ac9019a7
Danke1732 <s0lfa1732@gmail.com>
2020-11-12 15:47:41 +0900

    購入機能のモデルの単体テストコード作成

---
+++ spec/factories/transactions.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :transaction do
+    postal_code { "123-4567" }
+    prefecture_id { Faker::Number.within(range: 2..48) }
+    city { Gimei.city.kanji }
+    address { Faker::Address.building_number }
+    building { "マンション111-1" }
+    phone_num { "09012345678" }
+    token     { "tok_abcdefghijk00000000000000000" }
+    user_id     { Faker::Number.number(digits: 8) }
+    item_id     { Faker::Number.number(digits: 8) }
+  end
+end
