b18f505677415b58fdf21f7dd70d495e4bd328f9
Danke1732 <s0lfa1732@gmail.com>
2020-11-12 12:37:25 +0900

    Formオブジェクト(モデル)の作成及び設定

---
+++ app/models/transaction.rb
@@ -0,0 +1,23 @@
+class Transaction
+
+  include ActiveModel::Model
+  attr_accessor :postal_code, :prefecture_id, :city, :address, :building, :phone_num, :token, :user_id, :item_id
+
+  NAME_REGEX_POSTAL = /\A\d{3}[-]\d{4}\z/
+  NAME_REGEX_PHONE = /\A\d{,11}\z/
+
+  with_options presence: true do
+    validates :token
+    validates :postal_code, format: { with: NAME_REGEX_POSTAL, message: "Input correctly" }
+    validates :prefecture_id, numericality: { other_than: 1, message: 'Select' }
+    validates :city
+    validates :address
+    validates :phone_num, numericality: { only_integer: true, message: "Input only number" }
+  end
+  validates :phone_num, format: { with: NAME_REGEX_PHONE, message: "Input only number" }
+
+  def save
+    buyer = Buyer.create(user_id: user_id, item_id: item_id)
+    Destination.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, building: building, phone_num: phone_num, buyer_id: buyer.id)
+  end
+end
