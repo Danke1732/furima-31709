609a40c287b524d30cef99690d8c62c8a54425b8
Danke1732 <s0lfa1732@gmail.com>
2020-11-17 16:05:03 +0900

    出品商品画像レビュー機能の実装

---
+++ app/javascript/preview.js
@@ -0,0 +1,26 @@
+if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
+  window.addEventListener('DOMContentLoaded', function() {
+    const ImageList = document.getElementById('image-content');
+  
+    document.getElementById('item-image').addEventListener('change', (e) => {
+      const imageBefore = document.querySelector('.review-image');
+      if (imageBefore) {
+        imageBefore.remove();
+      }
+
+      function createImageHTML(blob) {
+        const imageDisplay = document.createElement('div');
+        const image = document.createElement('img');
+        image.setAttribute('src', blob);
+        image.setAttribute('class', 'review-image');
+        imageDisplay.appendChild(image);
+        ImageList.appendChild(imageDisplay);
+      }
+
+      const file = e.target.files[0];
+      const blob = window.URL.createObjectURL(file);
+      
+      createImageHTML(blob);
+    });
+  });
+}
