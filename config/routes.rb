ac87c2e453c09351604488e23d2c01c864d4ed5d
Danke1732 <s0lfa1732@gmail.com>
2020-11-18 22:25:59 +0900

    マイページ実装（ルーティング）

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root to: 'items#index'
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: "users/registrations",
+  }
   resources :items do
     resources :transactions, only: [:index, :create]
   end
+  resources :users, only: :show
 end
