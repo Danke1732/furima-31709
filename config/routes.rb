b10a0c7f7af3de6bc6d113475de186190ea2a2b3
Danke1732 <s0lfa1732@gmail.com>
2020-11-20 11:21:34 +0900

    検索機能のルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   root to: 'items#index'
   devise_for :users, controllers: {
-    registrations: "users/registrations",
+    registrations: 'users/registrations',
   }
   resources :items do
     resources :transactions, only: [:index, :create]
+    collection do
+      get 'search'
+    end
   end
   resources :users, only: :show
 end
