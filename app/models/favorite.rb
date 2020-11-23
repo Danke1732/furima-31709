4ad4e3a2d64c43f2c0bea676d874e28b9a31ece4
Danke1732 <s0lfa1732@gmail.com>
2020-11-23 19:14:00 +0900

    rubocopにてコード修正

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,7 +2,7 @@ class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :item
 
-  with_options presence: true do 
+  with_options presence: true do
     validates :user
     validates :item
   end
