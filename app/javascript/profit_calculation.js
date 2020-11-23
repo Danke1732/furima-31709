9b334f5addcd629bd2d5c93af9cd34a6e0296f38
Danke1732 <s0lfa1732@gmail.com>
2020-11-23 18:48:37 +0900

    お気に入り機能(非同期通信)の実装及びjavascriptファイル手直し

--- app/javascript/profit_calculation.js
+++ app/javascript/profit_calculation.js
@@ -1,11 +1,13 @@
-function calculation() {
-  const price = document.getElementById('item-price');
-  const tax_price = document.getElementById('add-tax-price');
-  const profit = document.getElementById('profit');
+if (document.URL.match( /items/ ) && document.URL.match( /new/ ) || document.URL.match( /items/ ) && document.URL.match( /[0-9]/ ) && document.URL.match( /edit/ ) || document.URL.match( /items/ ) && document.URL.match( /[0-9]/ ) || document.URL.match( /items/ )) {
+  function calculation() {
+    const price = document.getElementById('item-price');
+    const tax_price = document.getElementById('add-tax-price');
+    const profit = document.getElementById('profit');
 
-  let calc_tax_price = price.value / 10;
+    let calc_tax_price = price.value / 10;
 
-  tax_price.innerHTML = Math.floor(calc_tax_price).toLocaleString();
-  profit.innerHTML = Math.floor(price.value - calc_tax_price).toLocaleString();
+    tax_price.innerHTML = Math.floor(calc_tax_price).toLocaleString();
+    profit.innerHTML = Math.floor(price.value - calc_tax_price).toLocaleString();
+  }
+  setInterval(calculation, 1000);
 }
-setInterval(calculation, 500);
